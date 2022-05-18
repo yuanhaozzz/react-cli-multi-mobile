1a4b32f9bf43b0b11f32e797ac2d9e3e430b2691
yuanhao <huzhen@inke.cn>
2022-05-18 11:38:11 +0800

    deploy

--- scripts/start.js
+++ scripts/start.js
@@ -20,7 +20,7 @@ const {
   createCompiler,
   prepareProxy,
   prepareUrls,
-} = require("react-dev-utils/WebpackDevServerUtils");
+} = require("../config/webpackDevServer.utils");
 const openBrowser = require("react-dev-utils/openBrowser");
 const semver = require("semver");
 const paths = require("../config/paths");
@@ -119,7 +119,8 @@ buildPage()
       }
 
       console.log(chalk.cyan("Starting the development server...\n"));
-      openBrowser(urls.localUrlForBrowser);
+
+      // openBrowser(urls.localUrlForBrowser);
     });
 
     ["SIGINT", "SIGTERM"].forEach(function (sig) {
